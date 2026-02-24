3ac9c4ea169508444787cc43844822177d21c500
TedoreGhlonti <tedore.ghlonti@gmail.com>
2026-02-24 06:48:54 +0000

    extensions

---
+++ extensions/extensions.py
@@ -0,0 +1,15 @@
+user = input('File name: ').strip().lower()
+if user.endswith(".gif"):
+    print("image/gif")
+elif user.endswith(".jpg") or user.endswith("jpeg"):
+    print("image/jpeg")
+elif user.endswith("png"):
+    print("image/png")
+elif user.endswith("pdf"):
+    print("application/pdf")
+elif user.endswith(".txt"):
+    print("text/plain") 
+elif user.endswith(".zip"):
+    print("application/zip")
+else:
+    print("application/octet-stream")
