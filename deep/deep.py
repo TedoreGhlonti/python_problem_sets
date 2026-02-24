508800b3aa78dac6d2ac6567e5115ded6fa61f4c
TedoreGhlonti <tedore.ghlonti@gmail.com>
2026-02-24 06:24:04 +0000

    deep

---
+++ deep/deep.py
@@ -0,0 +1,5 @@
+user = input("What's the answer to the great question of life, the universe and everuthing?: ").strip().lower()
+if user == "42" or user == "forty-two" or user == "forty two":
+    print("Yes")
+else: 
+    print("No")
